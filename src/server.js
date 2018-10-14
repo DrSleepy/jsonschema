96ed8272eadca8a5ffd030c45e115b4a8868540f
DrSleepy <m-subahi@hotmail.com>
2018-10-14 21:13:44 +0100

    Added failing on unknown properties

--- src/server.js
+++ src/server.js
@@ -7,15 +7,19 @@ const server = express();
 
 const data = {
   productId: 1,
-  productName: 'An ice sculpture',
+  productName: 'Sculpture1',
+  price: 1,
+  email: 'steve@bob.com',
   tags: ['cold', 'ice'],
   dimensions: {
     length: 7.0,
-    width: 12.0
+    width: 12.0,
+    height: 9
   }
 };
 
-console.log('----------');
-console.log(validate(data, createUserSchema));
+const result = validate(data, createUserSchema);
+
+console.log('-------------', result);
 
 server.listen(3111, () => console.log(`Server has started on port ${3111}`));
